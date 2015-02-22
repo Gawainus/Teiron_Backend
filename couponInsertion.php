693ba45b7dd28382422d4d23bb23671bbc541495
Yumen Cao <gawain@yumentsao.com>
2015-02-22 12:40:47 -0600

    mini and air merged

---
+++ couponInsertion.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Insertion Complete<br>
+Yunzi Serial Number: <?php echo $_POST["YunziSN"]; ?><br>
+CouponID: <?php echo $_POST["CouponID"]; ?><br>
+CouponTitle: <?php echo $_POST["CouponTitle"]; ?><br>
+CouponDetails: <?php echo $_POST["CouponDetails"]; ?><br>
+
+</body>
+</html>
